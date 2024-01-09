3b3b652b55961fb10eda51322bf94f9701bf4425
Brandon Worobi <79031043+BrandonWorobi@users.noreply.github.com>
2024-01-08 21:45:24 -0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+const connectWalletButton = document.getElementById("connect-wallet");
+
+connectWalletButton.addEventListener("click", () => {
+    // Implement wallet connection logic here (e.g., using Web3.js)
+});
